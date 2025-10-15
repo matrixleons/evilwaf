f3993752b20aa14b4d53ae5f15f4ad81d0a33974
matrixleons <codeleons724@gmail.com>
2025-10-15 18:33:15 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # setup.py
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
